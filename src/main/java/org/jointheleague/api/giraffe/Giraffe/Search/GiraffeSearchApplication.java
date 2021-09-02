b08c4a90900d2f8bf9eceff4c7ada277865778c1
Samson <kempiak@gmail.com>
2021-09-01 17:24:29 -0700

    initial

---
+++ src/main/java/org/jointheleague/api/giraffe/Giraffe/Search/GiraffeSearchApplication.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.giraffe.Giraffe.Search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiraffeSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiraffeSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jointheleague/api/giraffe/Giraffe/Search/GiraffeSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.giraffe.Giraffe.Search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiraffeSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
