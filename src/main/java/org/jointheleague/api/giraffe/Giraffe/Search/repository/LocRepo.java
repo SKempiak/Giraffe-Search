43ee21da9154b142c535abe9f315df60b11da337
Samson <kempiak@gmail.com>
2021-09-08 18:25:56 -0700

    requesting the data ip

--- src/main/java/org/jointheleague/api/giraffe/Giraffe/Search/repository/LocRepo.java
+++ src/main/java/org/jointheleague/api/giraffe/Giraffe/Search/repository/LocRepo.java
@@ -2,9 +2,19 @@ package org.jointheleague.api.giraffe.Giraffe.Search.repository;
 
 
 import org.springframework.stereotype.Repository;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Repository
 public class LocRepo {
+    private final WebClient webClient;
+private static final String baseUrl = "http://localhost:5000/v2/api-docs";
+
+    public LocRepo() {
+        webClient = WebClient
+                .builder()
+                .baseUrl(baseUrl)
+                .build();
+    }
 
     public String getResults(String query){
         return "Searching for DND stuff to " + query;
