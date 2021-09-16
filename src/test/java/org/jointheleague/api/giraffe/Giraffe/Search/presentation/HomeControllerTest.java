28a797604eae200aedd8ad9968bde461980f2c06
JoeJoePotato <34780073+JoeJoePotato@users.noreply.github.com>
2021-09-15 17:49:00 -0700

    Part of milestone 9

---
+++ src/test/java/org/jointheleague/api/giraffe/Giraffe/Search/presentation/HomeControllerTest.java
@@ -0,0 +1,11 @@
+package org.jointheleague.api.giraffe.Giraffe.Search.presentation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeControllerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+    }
+}
