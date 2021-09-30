1fd2c7c6042a0b66e055df97e378e04f627c99f9
RobloxYesCaillouNo <jaydenkudo@gmail.com>
2021-09-29 17:30:12 -0700

    Done

--- src/main/java/org/jointheleague/api/giraffe/Giraffe/Search/repository/LocRepo.java
+++ src/main/java/org/jointheleague/api/giraffe/Giraffe/Search/repository/LocRepo.java
@@ -4,5 +4,29 @@ import org.jointheleague.api.giraffe.Giraffe.Search.repository.dto.LocResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 
 public class LocRepo {
+    private final WebClient webClient;
+
+    private static final String baseUrl = "http://localhost:5000/v2/api-docs";
+
+    public LocRepo() {
+        webClient = WebClient
+                .builder()
+                .baseUrl(baseUrl)
+                .build();
+    }
+
+    public LocResponse getResults(String query) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("fo", "json")
+                        .queryParam("at", "results")
+                        .queryParam("q", query)
+                        .build()
+                ).retrieve()
+                .bodyToMono(LocResponse.class)
+                .block()
+                .getResults();
+    }
+
 
 }
