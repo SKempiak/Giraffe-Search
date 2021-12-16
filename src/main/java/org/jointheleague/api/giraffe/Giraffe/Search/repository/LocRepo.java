4ba4c41ddb91ec37b0cdd11f4b6f18649abe3a7e
Samson <kempiak@gmail.com>
2021-12-15 17:18:07 -0800

    potential fix for api calling

--- src/main/java/org/jointheleague/api/giraffe/Giraffe/Search/repository/LocRepo.java
+++ src/main/java/org/jointheleague/api/giraffe/Giraffe/Search/repository/LocRepo.java
@@ -6,14 +6,17 @@ import org.jointheleague.api.giraffe.Giraffe.Search.repository.dto.Result;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 @Repository
 public class LocRepo {
     private final WebClient webClient;
 
     private static final String baseUrl = "https://www.dnd5eapi.co/api/";
-    String category = "ability-scores";
+    List<String> Categories = Arrays.asList("skills/", "proficiencies/", "languages/", "alignment/", "backgrounds/", "classes/", "subclasses/", "features/", "races/", "subraces/", "traits/", "equipment-categories/", "equipment/", "magic-items/", "weapon-properties/", "spells/", "feats/", "monsters/", "conditions/", "damage-types/", "magic-schools/", "rules/", "rule-sections/");
     public LocRepo() {
 
         webClient = WebClient
@@ -26,15 +29,25 @@ public class LocRepo {
     }
 
     public List<Result> getResults(String query) {
-        return webClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path(category + "/")
-                        .build()
-                ).retrieve()
-                .bodyToMono(LocResponse.class)
-                .block()
-                .getResults();
+        for (int i = 0; i < Categories.size(); i++) {
+            if(query.contains(spaceToDash(Categories.get(i).substring(0, Categories.get(i).length()-2)))) {
+                int iRemastered = i;
+                return webClient.get()
+                        .uri(uriBuilder -> uriBuilder
+                                .path(Categories.get(iRemastered))
+                                .build()
+                        ).retrieve()
+                        .bodyToMono(LocResponse.class)
+                        .block()
+                        .getResults();
+            }
+        }
+        return null;
     }
-
+    public String spaceToDash(String query) {
+        query = query.toLowerCase(Locale.ROOT);
+        query = query.replaceAll(" ", "-");
+        return query;
+    }
 
 }
